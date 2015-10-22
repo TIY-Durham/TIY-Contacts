40b55b070071ec9832821e5955aab602dc207e19
David Rogers <al.the.x@gmail.com>
2015-10-22 10:12:33 -0400

    Add resource routing and make EmailAddress embedded in Contact

--- api/app/models/contact.rb
+++ api/app/models/contact.rb
@@ -1,4 +1,19 @@
 class Contact < ActiveRecord::Base
   has_many :phone_numbers
   has_many :email_addresses
+
+  def as_json(arg)
+    {id: id,
+     first_name: first_name,
+     last_name: last_name,
+     email_addresses: email_addresses.map do |e|
+       {
+         id: e.id,
+         email_address: e.email_address,
+         email_type: e.email_type
+       }
+
+     end
+    }
+  end
 end
